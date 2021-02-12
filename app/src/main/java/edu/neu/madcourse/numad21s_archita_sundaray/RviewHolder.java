4047d58b16b9c33d62020b0c6af6833e7a82adb7
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-02-12 12:02:35 -0500

    completed RviewHolder class

--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/RviewHolder.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/RviewHolder.java
@@ -1,4 +1,45 @@
 package edu.neu.madcourse.numad21s_archita_sundaray;
 
-public class RviewHolder {
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RviewHolder extends RecyclerView.ViewHolder {
+    public TextView itemName;
+    public TextView itemDesc;
+    public CheckBox checkBox;
+
+    public RviewHolder(View itemView, final ItemClickListener listener){
+        super(itemView);
+        itemName = itemView.findViewById(R.id.item_name);
+        itemDesc = itemView.findViewById(R.id.item_desc);
+        checkBox = itemView.findViewById(R.id.checkbox);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    int position  = getLayoutPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(position);
+                    }
+                }
+            }
+        });
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    int position = getLayoutPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        listener.onCheckBoxClick(position);
+                    }
+                }
+            }
+        });
+        
+    }
 }
