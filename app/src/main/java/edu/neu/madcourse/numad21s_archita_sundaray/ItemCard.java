1183a7f62f0aa9dad2fea85a5bba32af688fe85e
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-02-12 11:23:16 -0500

    completed the ItemCard class

--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/ItemCard.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/ItemCard.java
@@ -1,16 +1,35 @@
 package edu.neu.madcourse.numad21s_archita_sundaray;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 public class ItemCard implements ItemClickListener {
+    private final String itemName;
+    private final String itemDesc;
+    private boolean isChecked;
+
+    public ItemCard(String itemName, String itemDesc, boolean isChecked) {
+        this.itemName = itemName;
+        this.itemDesc = itemDesc;
+        this.isChecked = isChecked;
+    }
+
+    public String getItemName() {
+        return itemName + (isChecked ? "checked" : "unchecked");
+    }
+
+    public String getItemDesc() {
+        return itemDesc;
+    }
+
+    public boolean getStatus() {
+        return isChecked;
+    }
+
     @Override
     public void onItemClick(int position) {
-        
+        isChecked = !isChecked;
     }
 
     @Override
     public void onCheckBoxClick(int position) {
-
+        isChecked = !isChecked;
     }
 }
