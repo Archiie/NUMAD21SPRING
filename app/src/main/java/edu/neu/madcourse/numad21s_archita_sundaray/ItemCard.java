b60122f9b22f3eb320aa7d8bcc67e13ce8d00abc
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-02-12 11:13:21 -0500

    modifying the same files

--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/ItemCard.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/ItemCard.java
@@ -1,4 +1,16 @@
 package edu.neu.madcourse.numad21s_archita_sundaray;
 
-public class ItemCard {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ItemCard implements ItemClickListener {
+    @Override
+    public void onItemClick(int position) {
+        
+    }
+
+    @Override
+    public void onCheckBoxClick(int position) {
+
+    }
 }
--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/ItemClickListener.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/ItemClickListener.java
@@ -1,4 +1,8 @@
 package edu.neu.madcourse.numad21s_archita_sundaray;
 
 public interface ItemClickListener {
+
+    void onItemClick(int position);
+
+    void onCheckBoxClick(int position);
 }
