5162aa8f96b875932dc70000d3f9495d92c3358c
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-02-13 18:47:23 -0500

    moved code from MainActivity to LinkCollector

--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/LinkCollector.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/LinkCollector.java
@@ -1,12 +1,34 @@
 package edu.neu.madcourse.numad21s_archita_sundaray;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class LinkCollector extends AppCompatActivity {
+    //essential components needed for Recycler View: RecyclerView, Adapter, LayoutManager
+    private ArrayList<ItemCard> itemList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RviewAdapter rviewAdapter;
+    private RecyclerView.LayoutManager rLayoutManager;
+    private FloatingActionButton addButton;
+
+    private static final String NUMBER_OF_ITEMS = "NUMBER_OF_ITEMS";
+    private static final String KEY_OF_INSTANCE = "KEY_OF_INSTANCE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +36,137 @@ public class LinkCollector extends AppCompatActivity {
         setContentView(R.layout.activity_link_collector);
 
         //action listener to the button
+        //do I do this separately or can I add it to the above code?
+        init(savedInstanceState);
+        addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                int pos = 0;
+                addItem(pos);
+            }
+        });
 
+
+        //describing which gesture signifies what action is performed
+        //here, swiping left or right signifies deleting the entry
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT){
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                Snackbar.make(LinkCollector.this, recyclerView, "Deleted an item", Snackbar.LENGTH_SHORT).show();
+                int position = viewHolder.getLayoutPosition();
+                itemList.remove(position);
+                rviewAdapter.notifyItemRemoved(position);
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    //Retaining information on change in orientations
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        int size = itemList == null ? 0 : itemList.size();
+        outState.putInt(NUMBER_OF_ITEMS, size);
+
+        //need to generate unique key for each item
+        for (int i=0; i<size; i++) {
+            outState.putString(KEY_OF_INSTANCE + i + "0", itemList.get(i).getItemName());
+            outState.putString(KEY_OF_INSTANCE + i + "1", itemList.get(i).getItemDesc());
+            outState.putBoolean(KEY_OF_INSTANCE + i + "2", itemList.get(i).getStatus());
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    private void init(Bundle savedInstanceState){
+        initialItemData(savedInstanceState);
+        createRecyclerView();
+    }
+
+    private void initialItemData(Bundle savedInstanceState) {
+        //when the activity is not opened the first time
+        if (savedInstanceState != null && savedInstanceState.containsKey(NUMBER_OF_ITEMS)) {
+            if (itemList == null || itemList.size() == 0) {
+                int size = savedInstanceState.getInt(NUMBER_OF_ITEMS);
+                //retrieving keys that were stored in the instance
+                for (int i = 0; i < size; i++) {
+                    String itemName = savedInstanceState.getString(KEY_OF_INSTANCE + i + 0);
+                    String itemDesc = savedInstanceState.getString(KEY_OF_INSTANCE + i + 1);
+                    boolean isChecked = savedInstanceState.getBoolean(KEY_OF_INSTANCE + i + 2);
+
+                    //making sure that there are no duplicates like this: "XXX(checked)"
+                    if (isChecked) {
+                        itemName = itemName.substring(0, itemName.lastIndexOf("("));
+                    }
+                    ItemCard itemCard = new ItemCard(itemName, itemDesc, isChecked);
+                    itemList.add(itemCard);
+                }
+            }
+        }
+        //when the activity is opened for the first time
+        else {
+            ItemCard item1 = new ItemCard("Gmail", "Example Description", false);
+            ItemCard item2 = new ItemCard("Google", "Example Description", false);
+            ItemCard item3 = new ItemCard("Youtube", "Example Description", false);
+            itemList.add(item1);
+            itemList.add(item2);
+            itemList.add(item3);
+        }
+    }
+
+    private void createRecyclerView() {
+        rLayoutManager = new LinearLayoutManager(this);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        rviewAdapter = new RviewAdapter(itemList);
+
+        ItemClickListener itemClickListener = new ItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                itemList.get(position).onItemClick(position);
+                rviewAdapter.notifyItemChanged(position);
+            }
+
+            @Override
+            public void onCheckBoxClick(int position) {
+                itemList.get(position).onCheckBoxClick(position);
+                rviewAdapter.notifyItemChanged(position);
+            }
+        };
+        rviewAdapter.setOnItemClickListener(itemClickListener);
+        recyclerView.setAdapter(rviewAdapter);
+        recyclerView.setLayoutManager(rLayoutManager);
+    }
+
+    private void addItem(int position) {
+        itemList.add(position, new ItemCard("Dummy item", "ITEM id: " + Math.abs(new Random().nextInt(100000)), false));
+        Snackbar.make(LinkCollector.this, recyclerView, "Add an item", Snackbar.LENGTH_SHORT).show();
+        rviewAdapter.notifyItemInserted(position);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
