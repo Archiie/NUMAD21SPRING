b0d315da7f01ae6dc569454d419dd6c3098a5cd5
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-02-19 20:57:30 -0500

    using locationmanager

--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/Locator.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/Locator.java
@@ -1,14 +1,48 @@
 package edu.neu.madcourse.numad21s_archita_sundaray;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class Locator extends AppCompatActivity {
+public class Locator extends AppCompatActivity implements LocationListener {
+    private TextView textView;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locator);
+
+        textView = (TextView) findViewById(R.id.location_textview);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
