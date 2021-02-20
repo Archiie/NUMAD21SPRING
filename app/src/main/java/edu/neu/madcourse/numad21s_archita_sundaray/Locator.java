634a8e86315fee393c0dd09fc7369d236a862c50
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-02-20 16:28:32 -0500

    working on Locator Java

--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/Locator.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/Locator.java
@@ -1,14 +1,46 @@
 package edu.neu.madcourse.numad21s_archita_sundaray;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Locator extends AppCompatActivity {
+    Button btLocation;
+    TextView tvLatitude, tvLongitude;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locator);
+
+        btLocation = findViewById(R.id.bt_Location);
+        tvLatitude = findViewById(R.id.tv_latitude);
+        tvLongitude = findViewById(R.id.tv_longitude);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(
+                Locator.this
+        );
+        btLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(Locator.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(Locator.this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    //invoke the method when both permissions are granted
+                    getCurrentLocation();
+                }
+            }
+        });
     }
+
+    
 
 }
