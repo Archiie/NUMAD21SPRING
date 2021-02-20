b9201cb703bbe46c6d989180c02604ccd9f34e87
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-02-19 21:03:55 -0500

    added network permissions

--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/Locator.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/Locator.java
@@ -2,7 +2,11 @@ package edu.neu.madcourse.numad21s_archita_sundaray;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -19,11 +23,25 @@ public class Locator extends AppCompatActivity implements LocationListener {
         setContentView(R.layout.activity_locator);
 
         textView = (TextView) findViewById(R.id.location_textview);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
     }
 
     @Override
-    public void onLocationChanged(@NonNull Location location) {
-        
+    public void onLocationChanged(Location location) {
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        textView.setText("Longitude: " + longitude + "\n" + "Latitude: " + latitude);
     }
 
     @Override
