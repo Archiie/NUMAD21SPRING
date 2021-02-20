bf37832e76913d35f99b979a6ad49331838a08f2
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-02-19 19:47:34 -0500

    added empty activity for Locator

---
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/Locator.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21s_archita_sundaray;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Locator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locator);
+    }
+}
