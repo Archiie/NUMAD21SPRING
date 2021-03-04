d74dc33384f9ea586440892ae8e65446d3f0422f
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-03-04 16:50:03 -0500

    completed WebServiceActivity class but error

--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/WebServiceActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/WebServiceActivity.java
@@ -2,9 +2,21 @@ package edu.neu.madcourse.numad21s_archita_sundaray;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class WebServiceActivity extends AppCompatActivity {
     private TextView mTitleTextView;
@@ -15,5 +27,78 @@ public class WebServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_service);
+
+        mTitleTextView = (TextView)findViewById(R.id.result_textview);
+        mURLEditText = (EditText)findViewById(R.id.URL_editText);
+    }
+
+    public void callWebserviceButtonHandler(View view){
+        PingWebServiceTask task = new PingWebServiceTask();
+        try {
+            String url = NetworkUtil.validInput(mURLEditText.getText().toString());
+            task.execute(url); // This is a security risk.  Don't let your user enter the URL in a real app.
+        } catch (NetworkUtil.MyException e) {
+            Toast.makeText(getApplication(),e.toString(),Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    // Google is deprecating Android AsyncTask API in Android 11 and suggesting to use java.util.concurrent
+    // But it is still important to learn & manage how it works
+    private class PingWebServiceTask  extends AsyncTask<String, Integer, JSONObject> {
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            Log.i(TAG, "Making progress...");
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+
+            JSONObject jObject = new JSONObject();
+            try {
+
+                // Initial website is "https://jsonplaceholder.typicode.com/posts/1"
+                URL url = new URL(params[0]);
+                String resp = NetworkUtil.httpResponse(url); // Get String response from the url address
+                //Log.i("resp",resp);
+
+                // JSONArray jArray = new JSONArray(resp);    // Use this if your web service returns an array of objects.  Arrays are in [ ] brackets.
+                // Transform String into JSONObject
+                jObject = new JSONObject(resp);
+
+                //Log.i("jTitle",jObject.getString("title"));
+                //Log.i("jBody",jObject.getString("body"));
+                return jObject;
+
+            } catch (MalformedURLException e) {
+                Log.e(TAG,"MalformedURLException");
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                Log.e(TAG,"ProtocolException");
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e(TAG,"IOException");
+                e.printStackTrace();
+            } catch (JSONException e) {
+                Log.e(TAG,"JSONException");
+                e.printStackTrace();
+            }
+
+            return jObject;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jObject) {
+            super.onPostExecute(jObject);
+            TextView result_view = (TextView)findViewById(R.id.result_textview);
+
+            try {
+                result_view.setText(jObject.getString("title"));
+            } catch (JSONException e) {
+                result_view.setText("Something went wrong!");
+            }
+
+        }
     }
 }
