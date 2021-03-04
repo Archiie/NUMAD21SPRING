ca82ce2035ad5800129da40fa68a96e38dcb6cd2
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-03-03 23:19:38 -0500

    modified AtYourService java class

--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/AtYourService.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/AtYourService.java
@@ -2,7 +2,9 @@ package edu.neu.madcourse.numad21s_archita_sundaray;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AtYourService extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class AtYourService extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_at_your_service);
+    }
+
+    public void startLaunchWebActivity(View view) {
+        startActivity(new Intent(AtYourService.this, LaunchWebActivity.class));
     }
 }
