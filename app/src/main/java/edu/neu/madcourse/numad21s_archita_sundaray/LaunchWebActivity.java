3423524bd080e6a8518a0c91c2622c0deb3dcf82
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-03-03 23:43:08 -0500

    completed Launch Web

--- app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/LaunchWebActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/LaunchWebActivity.java
@@ -2,15 +2,34 @@ package edu.neu.madcourse.numad21s_archita_sundaray;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LaunchWebActivity extends AppCompatActivity {
+    private EditText mUrlEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_web);
+
+        mUrlEditText = (EditText)findViewById(R.id.web_dest_edit_text);
     }
 
+    public void launchWeb(View view) {
+        String url = mUrlEditText.getText().toString();
+        try {
+            url = NetworkUtil.validInput(url);
+            Uri uri = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        } catch (NetworkUtil.MyException e) {
+            Toast.makeText(getApplication(), e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
