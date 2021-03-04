1d57b817768a91eab1b6c0c4e3a3784938bb6087
archita-sundaray <59573837+archita-sundaray@users.noreply.github.com>
2021-03-03 23:18:43 -0500

    added LaunchWebActivity

---
+++ app/src/main/java/edu/neu/madcourse/numad21s_archita_sundaray/LaunchWebActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21s_archita_sundaray;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LaunchWebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_web);
+    }
+}
